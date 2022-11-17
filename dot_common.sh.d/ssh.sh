a8c68e453c27645021a40f96691cfd33055bb27e
alexander haag <alexander_haag@gmx.de>
2022-11-17 12:10:25 +0100

    Add dynamic ssh load

--- dot_common.sh.d/ssh.sh
+++ dot_common.sh.d/ssh.sh
@@ -1,11 +1,18 @@
-if [[ -f "$HOME/.ssh/id_ed25519" ]]; then
-  case "$(uname)" in
-      "Darwin")
-          eval "$(ssh-agent -s)"
-          ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-          ;;
-      "Linux")
-          eval "$(ssh-agent -s)"
-          ssh-add ~/.ssh/id_ed25519
-  esac
+if [[ -d "${HOME}/.ssh/" ]]; then
+    COUNT_KEYS=$(find "${HOME}/.ssh" -type f -maxdepth 1 -name "*.pem")
+    if [[ "${#COUNT_KEYS[@]}" != 0 ]]; then
+        eval "$(ssh-agent -s)"
+        case "$(uname)" in
+            "Darwin")
+                for KEY in "${HOME}"/.ssh/*.pem; do
+                    ssh-add --apple-use-keychain "$KEY"
+                done
+                ;;
+            "Linux")
+                for KEY in "${HOME}"/.ssh/*.pem; do
+                    ssh-add "$KEY"
+                done
+                ;;
+        esac
+    fi
 fi
