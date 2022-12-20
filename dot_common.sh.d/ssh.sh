36f631727983aa09d5c245a9dd7cded6d189af3e
alexander haag <alexander_haag@gmx.de>
2022-12-20 08:25:21 +0100

    Improve scripts

--- dot_common.sh.d/ssh.sh
+++ dot_common.sh.d/ssh.sh
@@ -1,18 +1,20 @@
-if [[ -d "${HOME}/.ssh/" ]]; then
-    COUNT_KEYS=$(find "${HOME}/.ssh" -type f -maxdepth 1 -name "*.pem")
-    if [[ "${#COUNT_KEYS[@]}" != 0 ]]; then
-        eval "$(ssh-agent -s)"
-        case "$(uname)" in
-            "Darwin")
-                for KEY in "${HOME}"/.ssh/*.pem; do
-                    ssh-add --apple-use-keychain "$KEY"
-                done
-                ;;
-            "Linux")
-                for KEY in "${HOME}"/.ssh/*.pem; do
-                    ssh-add "$KEY"
-                done
-                ;;
-        esac
-    fi
+if [[ ! -d "${HOME}/.ssh/" ]]; then
+    exit
 fi
+COUNT_KEYS=$(find "${HOME}/.ssh" -type f -maxdepth 1 -name "*.pem")
+if [[ "${#COUNT_KEYS[@]}" == 0 ]]; then
+    exit
+fi
+eval "$(ssh-agent -s)"
+case "$(uname)" in
+    "Darwin")
+        for KEY in "${HOME}"/.ssh/*.pem; do
+            ssh-add --apple-use-keychain "$KEY"
+        done
+        ;;
+    "Linux")
+        for KEY in "${HOME}"/.ssh/*.pem; do
+            ssh-add "$KEY"
+        done
+        ;;
+esac
