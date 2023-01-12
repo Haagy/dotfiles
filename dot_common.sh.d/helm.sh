7d4bb71e902a01347644f5b94489723935dc8b46
alexander haag <alexander_haag@gmx.de>
2023-01-12 15:21:05 +0100

    add helm aliases

--- dot_common.sh.d/helm.sh
+++ dot_common.sh.d/helm.sh
@@ -1,6 +1,8 @@
 if command -v helm &> /dev/null
 then
+    export NS=default
     alias ht='helm template'
+    alias hui='helm upgrade --install --create-namespace --namespace $NS'
 
     source <(helm completion kind)
 fi
