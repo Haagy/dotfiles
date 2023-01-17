741009de816275bf3180071c19256a3fe4c3e4a8
alexander haag <alexander_haag@gmx.de>
2023-01-17 16:04:05 +0100

    fix helm completion

--- dot_common.sh.d/helm.sh
+++ dot_common.sh.d/helm.sh
@@ -4,5 +4,5 @@ then
     alias ht='helm template'
     alias hui='helm upgrade --install --create-namespace --namespace $NS'
 
-    source <(helm completion kind)
+    source <(helm completion zsh)
 fi
