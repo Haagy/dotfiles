0dc2545ddafc606308ed6220afb90e4dd8c890c4
alexander haag <alexander_haag@gmx.de>
2023-03-17 08:53:27 +0100

    update aws profiles

--- dot_common.sh.d/aws.sh
+++ dot_common.sh.d/aws.sh
@@ -1,4 +1,7 @@
 if command -v aws &> /dev/null
 then
     export AWS_PAGER=""
+    export AWS_PROFILE=wepf3
+    export AWS_DEFAULT_PROFILE="wepf3"
+    export AWS_DEFAULT_REGION="eu-central-1"
 fi
--- dot_common.sh.d/terraform.sh
+++ dot_common.sh.d/terraform.sh
@@ -2,7 +2,7 @@ if command -v terraform &> /dev/null
 then
     alias tf='terraform'
     terraform --install-autocomplete
-    export AWS_PROFILE=wepf
+    export AWS_PROFILE=wepf3
 fi
 
 if command -v terragrunt &> /dev/null
